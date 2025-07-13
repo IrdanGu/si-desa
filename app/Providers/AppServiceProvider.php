443e9b9dd64450b8a6d8db09b38a3d4864a8097b
Irdan Guntara <67906872+irza1@users.noreply.github.com>
2025-07-14 01:12:55 +0700

    update code production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,30 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // 1. Tambahkan baris ini
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
         //
     }
 
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        // 2. Tambahkan blok "if" ini
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
